1047500c1781f656eb4329da2dfbdd263dded160
Joshua Payne <payne@Joshuas-MacBook-Pro.local>
2025-07-09 16:41:48 -0700

    init

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,36 @@
+return {
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            local configs = require("nvim-treesitter.configs")
+            configs.setup({
+                -- enable syntax highlighting
+                highlight = {
+                    enable = true,
+                },
+                -- enable indentation
+                indent = { enable = true },
+                -- enable autotagging (w/ nvim-ts-autotag plugin)
+                autotag = { enable = true },
+                -- ensure these language parsers are installed
+                ensure_installed = {
+                    "json",
+                    "javascript",
+		    "java",
+                    "typescript",
+                    "html",
+                    "css",
+                    "markdown",
+                    "bash",
+                    "lua",
+                    "vim",
+                    "c",
+                    "gitignore",
+                },
+                -- auto install above language parsers
+                auto_install = false,
+            })
+        end
+    }
+}
