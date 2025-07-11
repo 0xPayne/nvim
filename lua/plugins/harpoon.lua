778ec60fb79e8b999fea6d3f5f3a985bbdf54440
Paynecoder <113269578+Paynecoder@users.noreply.github.com>
2025-07-11 16:53:55 -0700

    harpoon fix

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,25 +1,3 @@
-local conf = require('telescope.config').values
-local themes = require('telescope.themes')
-
--- helper function to use telescope on harpoon list.
-local function toggle_telescope(harpoon_files)
-    local file_paths = {}
-    for _, item in ipairs(harpoon_files.items) do
-        table.insert(file_paths, item.value)
-    end
-    local opts = themes.get_ivy({
-        promt_title = "Working List"
-    })
-
-    require("telescope.pickers").new(opts, {
-        finder = require("telescope.finders").new_table({
-            results = file_paths,
-        }),
-        previewer = conf.file_previewer(opts),
-        sorter = conf.generic_sorter(opts),
-    }):find()
-end
-
 return {
     "ThePrimeagen/harpoon",
     branch = "harpoon2",
@@ -31,8 +9,6 @@ return {
 	-- Keybinds
         vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
         vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-        vim.keymap.set("n", "<leader>fl", function() toggle_telescope(harpoon:list()) end,
-            { desc = "Open harpoon window" })
         vim.keymap.set("n", "<C-p>", function() harpoon:list():prev() end)
         vim.keymap.set("n", "<C-n>", function() harpoon:list():next() end)
     end
