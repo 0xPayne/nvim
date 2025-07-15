00e5068fb9c47c58cb0d2af68c19a92a126ee547
Joshua Payne <payne@Joshuas-MacBook-Pro.local>
2025-07-14 19:07:12 -0700

    cpp imports in leetcode automatically

--- lua/plugins/leetcode.lua
+++ lua/plugins/leetcode.lua
@@ -9,6 +9,34 @@ return {
 			"nvim-telescope/telescope.nvim",
 		},
 		lazy = leet_arg ~= vim.fn.argv(0, -1),
-		opts = { arg = leet_arg },
-	}
+		opts = {
+			arg = leet_arg,
+			injector = {
+				["cpp"] = {
+					imports = function()
+						return {
+							"#include <iostream>",
+							"#include <vector>",
+							"#include <string>",
+							"#include <algorithm>",
+							"#include <unordered_map>",
+							"#include <unordered_set>",
+							"#include <queue>",
+							"#include <stack>",
+							"#include <set>",
+							"#include <map>",
+							"#include <cmath>",
+							"#include <numeric>",
+							"#include <climits>",
+							"#include <cstring>",
+							"#include <functional>",
+							"#include <memory>",
+							"#include <cassert>",
+							"using namespace std;"
+						}
+					end,
+				},
+			},
+		}
+	},
 }
