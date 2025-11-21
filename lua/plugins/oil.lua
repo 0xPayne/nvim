cb56f352ce9c9defca7df41c753d2eb970ac3895
0xPayne <113269578+0xPayne@users.noreply.github.com>
2025-11-21 11:00:35 -0800

    ignore dsstore

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,13 +1,21 @@
 return {
-    {
-	"stevearc/oil.nvim",
-	dependencies = {
-	    "nvim-tree/nvim-web-devicons",
-	},
-	config = function ()
-	   require("oil").setup(
-	    { view_options = { show_hidden = true, }}
-	    )
-	end,
-    }
+	{
+		"stevearc/oil.nvim",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			require("oil").setup(
+				{
+					view_options = {
+						show_hidden = true,
+						is_hidden_file = function(name, bufnr)
+							return vim.startswith(name, ".") or name == ".DS_Store"
+						end,
+					}
+
+				}
+			)
+		end,
+	}
 }
